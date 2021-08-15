cc2a76500916a1998068a60044a470eb23107547
jetxeberria <jetxebemen@gmail.com>
2021-08-15 20:05:38 +0200

    Skip manual test #0

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,12 @@
 
 """Test flask app."""
 
+import pytest
+
 from meme_generator.app import main
 
 
+@pytest.mark.skip(reason="Manual test to run server app.")
 def test_run_app_given_app_when_run_then_rendered():
     main()
     assert 0
